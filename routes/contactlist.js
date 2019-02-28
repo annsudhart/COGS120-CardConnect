7d1d7161bf1f147364b574101e4757e8e0b9db5a
Qi Goh <qgoh@ucsd.edu>
2019-02-28 02:34:59 -0800

    dynamic svg & data consistency

--- routes/contactlist.js
+++ routes/contactlist.js
@@ -9,16 +9,19 @@ exports.view = function(request, response) {
 exports.addContact = function(request, response) {
 	var name = request.body.name;
 	var newContact = {
-			"fullName": request.body.name,
+			"fullName": request.body.first + " " + request.body.last,
+			"first": request.body.first,
+			"last": request.body.last,
 			"phone": request.body.phone,
 			"email": request.body.email,
-			"address": request.body.address,
+			"line1": request.body.line1,
+			"line2": request.body.line2,
 			"company": request.body.company,
 			"website": request.body.website,
 			"image": "placeholder.svg"
 		}
 	data.contact.push(newContact);
-
+	
 	response.render('contactlist', {
 		"data": data
 	});
--- routes/existingcontact.js
+++ routes/existingcontact.js
@@ -21,10 +21,13 @@ exports.saveContact = function(request, response) {
 	var output;
 	for (k=0; k<data.contact.length; k++) {
 		if (data.contact[k].fullName == name) {
-			data.contact[k].fullName = request.body.name;
+			data.contact[k].fullName = request.body.first + " " + request.body.last; 
+			data.contact[k].first = request.body.first;
+			data.contact[k].last = request.body.last;
 			data.contact[k].phone = request.body.phone;
 			data.contact[k].email = request.body.email;
-			data.contact[k].address = request.body.address;
+			data.contact[k].line1 = request.body.line1;
+			data.contact[k].line2 = request.body.line2;
 			data.contact[k].company = request.body.company;
 			data.contact[k].website = request.body.website;
 			output = data.contact[k];
