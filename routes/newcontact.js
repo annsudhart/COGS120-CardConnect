2074b9a07f8ffbcfae402d45882892ad852d4a54
Qi Goh <qgoh@ucsd.edu>
2019-03-08 15:17:10 -0800

    test

--- routes/newcontact.js
+++ routes/newcontact.js
@@ -12,7 +12,7 @@ exports.processImage = function(request, response) {
 	var url = request.body.url;
 	response.render("newcontactB", {
 		"url": url, 
-		"img": "false"
+		"img": true
 	}); 
 	console.log(url);
 };
